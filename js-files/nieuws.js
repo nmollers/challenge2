9a234d70dea9cf5d8b86827641f3c06fa4979cc8
nmollers <210981@vistacollege.nl>
2022-09-30 15:00:31 +0200

    .

---
+++ js-files/nieuws.js
@@ -0,0 +1,44 @@
+// Splash Screen JS Code
+let intro = document.querySelector('.intro');
+let logo = document.querySelector('.logo-header');
+let logoSpan = document.querySelectorAll('.logo');
+
+window.addEventListener('DOMContentLoaded', ()=>{
+
+    setTimeout(()=>{
+
+        logoSpan.forEach((span, idx)=>{
+            setTimeout(()=>{
+                span.classList.add('active');
+            }, (idx + 1) * 400)
+        });
+
+        setTimeout(()=>{
+            logoSpan.forEach((span, idx)=>{
+
+                setTimeout(()=>{
+                    span.classList.remove('active');
+                    span.classList.add('fade');
+                }, (idx + 1) * 50)
+            })
+        },2000);
+
+        setTimeout(()=>{
+            intro.style.top = '-100vh';
+        }, 2300)
+    })
+})
+
+// Nav Menu
+function myFunction(x) {
+    x.classList.toggle("change");
+  }
+
+// content menu 
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+  }
