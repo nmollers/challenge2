38b7b9a9d7a189f9de883b52a87ef5e6b4b0eed1
BruceVista <513077@vistacollege.nl>
2022-10-03 13:04:41 +0200

    Het Team
    pagina gemaakt met alles

---
+++ js-files/team.js
@@ -0,0 +1,13 @@
+// Nav Menu
+function myFunction(x) {
+    x.classList.toggle("change");
+  }
+
+// content menu 
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+  }
